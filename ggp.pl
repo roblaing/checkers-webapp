61ea6e6d5e84d2cc0471467d2a7e4f4135dfe486
roblaing <robert.laing@rocketmail.com>
2017-07-16 14:24:22 +0200

    Advanced to minimax

--- ggp.pl
+++ ggp.pl
@@ -13,13 +13,21 @@ findinits(Inits) :-
 findlegals(Role, State, Legals) :-
     update_state(State),
     setof(does(Role, Legal), legal(Role, Legal), Legals).
+    
+findlegals(Role, Legals) :-
+    setof(does(Role, Legal), legal(Role, Legal), Legals).
 
 findnext(Does, State, Next) :-
     update_state(State),
     retractall(does(_,_)),
     assertz(Does),
     setof(Proposition, next(Proposition), Next).
-    
+
+findnext(Does, Next) :-  % State already set
+    retractall(does(_,_)),
+    assertz(Does),
+    setof(Proposition, next(Proposition), Next).
+
 findterminalp(State, Terminal) :-
     update_state(State),
     ((terminal, Terminal = true) ; 
@@ -29,6 +37,7 @@ findreward(Role, State, Reward) :-
     update_state(State),
     goal(Role, Reward).
 
+/*
 % create a random player to test UI
 
 random_move(State, Move) :-
@@ -36,49 +45,83 @@ random_move(State, Move) :-
     true(control(Role)),
     setof(does(Role, Legal), legal(Role, Legal), Legals),
     random_member(Move, Legals).
+*/
 
+maxplayer(black).
+timelimit(10).
 
-% bestmove(State, Move)
+triple(X, Y, Z, Result) :-
+    Result = [X, Y, Z].
+    
+prod(X, Y, Result) :-
+    Result is X * Y.
+    
+add(X, Y, Result) :-
+    Result is X + Y.
 
-aiplayer(black).
+eq([Maxval,_],[Maxval,_]).
 
-idf*([State|Open1], Depth1, Limit, Value) :-
-    update_state(State),
-    (\+terminal,
-     Depth1 < Limit,
-     true(control(Role)),
-     findlegals(Role, State, Legals),
-     findall(Next, (member(Legal, Legals), findnext(Legal, State, Next)), Nexts),
-     append(Nexts, Open1, Open2),
-     Depth2 is Depth1 + 1,
-     idf*(Open2, Depth2, Limit, Value))
-    ;
-    (terminal,
-     goal(red, Red),
-     goal(black, Black),
-     Value is Black - Red)
-    ;
-    (\+terminal,
-     Depth1 == Limit,
-     true(piece_count(red, Red)),
-     true(piece_count(black, Black)),
-     Value is Black - Red).
+utility(Utility) :-
+    ((terminal,
+      goal(black, Black),
+      goal(red, Red),
+      Utility is Black - Red)
+     ;
+     (\+terminal,
+      true(piece_count(black, Black)),
+      true(piece_count(red, Red)),
+      Utility is Black - Red)).    
 
-valuemove(State, Move, Value) :-
-    % aiplayer(Role),
-    update_state(State),
-    findnext(Move, State, Next),
-    idf*([Next], 0, 80, Value).
+get_children(Children) :- % update_state(Node) has already been called
+    true(control(Role)),
+    findlegals(Role, Legals),
+    maplist(findnext, Legals, Children).
 
-maxval(Val, Val/_).
+timeouts(Moves, Timeouts) :-
+    get_time(Now),
+    length(Moves, Length),
+    timelimit(Timelimit),
+    Timeout is Timelimit / Length,
+    findall(X, between(1, Length, X), Timeouts0),
+    maplist(prod(Timeout), Timeouts0, Timeouts1),
+    maplist(add(Now), Timeouts1, Timeouts).
 
-bestmove(State, Action) :-
+minimax(Node, Timeout, BestValue) :-
+    update_state(Node),
+    get_time(Now),
+    (((terminal ; Now >= Timeout),
+      utility(BestValue))
+     ;
+     (\+terminal,
+      Now < Timeout,
+      get_children(Children),
+      findall(BestValue1, 
+            (member(Pos, Children),
+             minimax(Pos, Timeout, BestValue1)), 
+            BestValues),
+      update_state(Node),
+      maxplayer(Maxplayer),
+      true(control(Role)),
+      ((Maxplayer == Role,
+        max_list(BestValues, BestValue))
+       ;
+       (Maxplayer \== Role,
+        min_list(BestValues, BestValue)))
+     )).
+
+bestmove(State, Move) :-
     update_state(State),
     true(control(Role)),
-    findlegals(Role, State, Legals),
-    setof(Value/Legal, (member(Legal, Legals), valuemove(State, Legal, Value)), Values1),
-    reverse(Values1, Values2),
-    [MaxVal/_|_] = Values2,
-    sublist(maxval(MaxVal), Values2, Values3),
-    random_member(_/Action, Values3).
+    findlegals(Role, Legals),
+    timeouts(Legals, Timeouts),
+    maplist(findnext, Legals, Nexts),
+    maplist(triple, Legals, Nexts, Timeouts, LegalNextList),
+    findall([Value, Legal], (member([Legal, Next, Timeout], LegalNextList), minimax(Next, Timeout, Value)), ValueLegalList1),
+    sort(1, @>=, ValueLegalList1, ValueLegalList2),
+    ValueLegalList2 = [[Maxval,_]|_],
+    include(eq([Maxval,_]), ValueLegalList2, ValueLegalList),
+    random_member(ValueLegal, ValueLegalList),
+    [_, Move] = ValueLegal.
+
+
 

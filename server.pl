f853e5e7204769f8aa83c910b7ad8e24d2669413
roblaing <robert.laing@rocketmail.com>
2018-04-16 17:39:58 +0200

    Fixed bug in game over (hopefully)

--- server.pl
+++ server.pl
@@ -1,4 +1,3 @@
-% swipl server.pl --port=20553 --pidfile=http.pid
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_unix_daemon)).
@@ -17,19 +16,19 @@ move_handler(Request) :-
     term_string(State, StateString),
     term_string(AIplayer, AIplayerString),
     findnext(Move, State, NextState),
-    findterminalp(NextState, Terminal),
-    ((\+Terminal,
-    % random_move(NextState, AIMove),
-    bestmove(NextState, AIMove),
-    findnext(AIMove, NextState, Next),
-    findlegals(red, Next, Legals))
+    findterminalp(NextState, HumanWin),
+    (\+HumanWin, !,
+    (bestmove(NextState, AIMove),
+     findnext(AIMove, NextState, Next),
+     findlegals(red, Next, Legals),
+     findreward(red, Next, Reward),
+     findterminalp(Next, Terminal),
+     format('Content-type: application/x-www-form-urlencoded~n~n', []),
+     format('move=~w&state=~w&legals=~w&aiplayer=~w&reward=~w&terminal=~w', [AIMove, Next, Legals, AIplayer, Reward, Terminal]))     
     ;
-    (Terminal,
-     Next = NextState,  
-     Legals = noop)),
-    findreward(red, NextState, Reward),
+    (findreward(AIplayer, NextState, Reward),
     format('Content-type: application/x-www-form-urlencoded~n~n', []),
-    format('move=~w&state=~w&legals=~w&aiplayer=~w&reward=~w&terminal=~w', [AIMove, Next, Legals, AIplayer, Reward, Terminal]).
+    format('move=~w&state=~w&legals=~w&aiplayer=~w&reward=~w&terminal=~w', [AIMove, NextState, 'noop', AIplayer, Reward, HumanWin]))).
 
 main :-
   consult('checkers.pl'),
